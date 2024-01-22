88b271ff79c4abd673867842acaeb821b787c0d9
Martin Koldodovsky <mkoldov@gmail.com>
2024-01-22 15:28:15 +0100

    config

--- api/config.ts
+++ api/config.ts
@@ -3,12 +3,11 @@ import { load } from "https://deno.land/std@0.210.0/dotenv/mod.ts";
 const env = await load();
 
 const config = {
-  ghToken: env.GH_TOKEN,// env.GIT_TOKEN,
-  ghRepoOwner: env.REPO_OWNER,
-  ghRepoName: env.REPO_NAME,
-  ghRepoFile: env.REPO_FILE,
-  ghRepoBranch: env.REPO_BRANCH,
-  ghRepoPlanBranch: env.REPO_PLAN_BRANCH,
+  ghToken: env.GH_TOKEN || Deno.env.get("GH_TOKEN"),
+  ghRepoOwner: env.REPO_OWNER || Deno.env.get("REPO_OWNER"),
+  ghRepoFile: env.REPO_FILE || Deno.env.get("REPO_FILE"),
+  ghRepoBranch: env.REPO_BRANCH || Deno.env.get("REPO_BRANCH"),
+  ghRepoPlanBranch: env.REPO_PLAN_BRANCH || Deno.env.get("REPO_PLAN_BRANCH"),
 //   ciLogfile: env.CI_LOGFILE,
 //   ciLogfilePostfix: env.CI_LOGFILE_POSTFIX,
 //   ciWorkflowName: env.CI_WORKFLOW_NAME,
